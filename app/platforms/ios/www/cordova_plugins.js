7645544f1c37c3bf872ca7f42e832d9a7af41595
Gerard Bayego <gerardbayego@MacBook-Pro-de-Gerard.local>
2015-12-19 19:12:43 +0000

    Render IMGs

--- app/platforms/ios/www/cordova_plugins.js
+++ app/platforms/ios/www/cordova_plugins.js
@@ -1,9 +1,19 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/com.synconset.imagepicker/www/imagepicker.js",
+        "id": "com.synconset.imagepicker.ImagePicker",
+        "pluginId": "com.synconset.imagepicker",
+        "clobbers": [
+            "plugins.imagePicker"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.0"
+    "cordova-plugin-whitelist": "1.2.0",
+    "com.synconset.imagepicker": "1.0.7"
 }
 // BOTTOM OF METADATA
 });
