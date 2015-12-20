77286309ac183d469a0c87cc9b6f64ae723ef74d
Daniel Gil <danielgilsancho@gmail.com>
2015-12-20 02:36:13 +0000

    splash-screens + info hackathon

--- app/platforms/ios/www/langs/en.js
+++ app/platforms/ios/www/langs/en.js
@@ -5,7 +5,7 @@ strings.select_images = "PeterDrop&nbsp;&nbsp;🚀";
 strings.info = "Info";
 
 //INFORMATION PAGE
-strings.text0 = "Little more information";
+strings.text0 = "This superb app has been created completely in the Peterborough's hackathon (Hack the Holidays) in 2015!";
 
 			
 window.strings_en = strings;
