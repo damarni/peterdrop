ff4e99db5820cc30911969331fa802259f7bbfce
Daniel Gil <danielgilsancho@gmail.com>
2015-12-20 02:12:23 +0000

    info preparada

--- app/platforms/ios/www/langs/en.js
+++ app/platforms/ios/www/langs/en.js
@@ -4,6 +4,9 @@ var strings = {};
 strings.select_images = "PeterDrop&nbsp;&nbsp;🚀";
 strings.info = "Info";
 
+//INFORMATION PAGE
+strings.text0 = "Little more information";
+
 			
 window.strings_en = strings;
 
