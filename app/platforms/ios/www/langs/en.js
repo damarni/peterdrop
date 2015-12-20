1bde7aae118c73b73ba4e3d2847bd9898043887a
Daniel Gil <danielgilsancho@gmail.com>
2015-12-20 10:34:00 +0000

    arreglant imatges

--- app/platforms/ios/www/langs/en.js
+++ app/platforms/ios/www/langs/en.js
@@ -7,7 +7,6 @@ strings.info = "Info";
 //INFORMATION PAGE
 strings.text0 = "This superb app has been created completely in the Peterborough's hackathon (Hack the Holidays) in 2015!";
 
-			
 window.strings_en = strings;
 
 
